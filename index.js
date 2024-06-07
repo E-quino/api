c21de24e9488498fca8c6bde77308744c97b592e
CodyKoInABox <codykoinabox@gmail.com>
2024-06-07 00:52:49 -0300

    api setup

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// EXPRESS API SETUP
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8080
 
 
+// MYSQL DATABASE CONNECTION
+const mysql = require('mysql2')
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'equino'
+})
+
+// test
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
